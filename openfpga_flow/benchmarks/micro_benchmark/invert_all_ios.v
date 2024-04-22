3e2b7f159a92647e00c0db5cb050bcff0d9050c8
NouranAbdelaziz <nouran.abdelaziz@aucegpt.edu>
2024-04-22 14:43:15 +0200

    modified invert all ios design

--- openfpga_flow/benchmarks/micro_benchmark/invert_all_ios.v
+++ openfpga_flow/benchmarks/micro_benchmark/invert_all_ios.v
@@ -1,7 +1,7 @@
 module invert_all_ios(
   
-  input wire [11:0] in,
-  output wire [11:0] out
+  input wire [12:0] in,
+  output wire [13:0] out
 );
 
 assign out[0] = ~in[0];
@@ -16,5 +16,8 @@ assign out[8] = ~in[8];
 assign out[9] = ~in[9];
 assign out[10] = ~in[10];
 assign out[11] = ~in[11];
+
+assign out[12] = ~in[12];
+assign out[13] = 1'b1;
 
 endmodule
